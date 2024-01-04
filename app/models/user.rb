96856240dcd58bbe5ef75fb668a8b8267f325de4
saggeru <ylobb99@gmail.com>
2024-01-04 17:34:12 +0900

    エラーメッセージが重複する問題を解決

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, :birthday, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, presence: true, format: { with: PASSWORD_REGEX }, length: { minimum: 6 }
+  validates :password, presence: true, format: { with: PASSWORD_REGEX }
   validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_kana, :last_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
 end
