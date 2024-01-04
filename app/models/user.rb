d2f727fdf1a3d0d17445946f35f73435cedb2a5b
saggeru <ylobb99@gmail.com>
2024-01-04 15:44:48 +0900

    バリデーションを付与

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birthday, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, presence: true, format: { with: PASSWORD_REGEX }, length: { minimum: 6 }
+  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_kana, :last_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+end
