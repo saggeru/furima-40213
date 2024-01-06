be4b295ed6ae13e38390734ffed2bb91bcb20052
saggeru <ylobb99@gmail.com>
2024-01-06 22:10:37 +0900

    バリデーション 無効な記述を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,22 @@
 class Item < ApplicationRecord
 
-  validates :item_name, :about_item, :price, presence: true, unless: :was_attaced?
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_payment
+  belongs_to :shipping_day
+  belongs_to :prefecture
+
+
+  # -- バリデーション -- #
+  validates :item_name, :about_item, :price, presence: true, unless: :was_attached?
   validates :category_id, :condition_id, :shipping_fee_payment_id, :shipping_day_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   
   def was_attached?
     self.image.attached?
   end
-
-
-  # -- アソシエーション -- #
-  belongs_to :user
-  has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensinons
-  belongs_to :category, :condition, :shipping_fee_payment, :shipping_day, :prefecture
 
 end
