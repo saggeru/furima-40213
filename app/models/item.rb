0edf160446d6318af299b2ed985155436c6b361e
saggeru <ylobb99@gmail.com>
2024-01-06 20:34:14 +0900

    バリデーションを付与

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+
+  validates :item_name, :about_item, :price, presence: true
+  validates :category_id, :condition_id, :shipping_fee_payment_id, :shipping_day_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  
 end
