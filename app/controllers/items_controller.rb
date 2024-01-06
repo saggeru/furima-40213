f523e4de766dd0b6b435563188f0bc456d8d1c3b
saggeru <ylobb99@gmail.com>
2024-01-06 22:18:45 +0900

    new・createアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,20 @@
 class ItemsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
 end
