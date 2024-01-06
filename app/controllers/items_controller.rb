0920e644bc886d1504b63ce0a30fabe927ee5a5f
saggeru <ylobb99@gmail.com>
2024-01-06 22:19:07 +0900

    ストロングパラメーターの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -16,5 +16,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :item_name, :about_item, :price, :category_id, :condition_id, :shipping_fee_payment_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
 
 end
