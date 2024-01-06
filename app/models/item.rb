70e1537a1da086ba1cc12f9646c73a1583974660
saggeru <ylobb99@gmail.com>
2024-01-06 20:47:58 +0900

    ActiveStorageにて画像をアップロード可能へ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,17 @@
 class Item < ApplicationRecord
 
-  validates :item_name, :about_item, :price, presence: true
+  validates :item_name, :about_item, :price, presence: true, unless: :was_attaced?
   validates :category_id, :condition_id, :shipping_fee_payment_id, :shipping_day_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  
+  def was_attached?
+    self.image.attached?
+  end
 
+
+  # -- アソシエーション -- #
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensinons
   belongs_to :category, :condition, :shipping_fee_payment, :shipping_day, :prefecture
+
 end
