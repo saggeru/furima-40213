462f0fbdcedf79ee9853c1347efac240edaf58f4
saggeru <ylobb99@gmail.com>
2024-01-06 23:18:39 +0900

    販売価格の最低～上限を設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
 
   # -- バリデーション -- #
   validates :item_name, :about_item, :price, :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   validates :category_id, :condition_id, :shipping_fee_payment_id, :shipping_day_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
