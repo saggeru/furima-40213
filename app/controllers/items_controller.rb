26682754980391ae291c99e743037b730ea7fbfb
saggeru <ylobb99@gmail.com>
2024-01-08 18:28:25 +0900

    未ログイン状態で編集ページへ遷移できないよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
