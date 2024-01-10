83cf6a8377567023e16b953d50c3f3e62ac64433
saggeru <ylobb99@gmail.com>
2024-01-11 01:56:55 +0900

    address_no(番地) 正規表現は不要と感じたので修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,10 +5,9 @@ class OrderAddress
 
   # ▼ バリデーション ▼ #
   with_options presence: true do
-    validates :user_id, :item_id
+    validates :address_no, :user_id, :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Only enter in japanese"}
-    validates :address_no, format: {with: /\A[ぁ-んァ-ン一-龥0-9０-９]+\z/, message: "is invalid."}
     validates :phone_number, format: {with: /\A[0-9]+\z/, length: 10..13}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   end
