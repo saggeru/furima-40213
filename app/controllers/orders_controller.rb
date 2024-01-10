2b1fb5bfa5b61399e9d033766366305ded4d4bc4
saggeru <ylobb99@gmail.com>
2024-01-11 01:09:42 +0900

    Createアクションを定義 及びストロングパラメーターの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,24 @@ class OrdersController < ApplicationController
   
   def index
     @item = Item.find_by(id: params[:item_id])
+    # ▲ item_id を取得 Formオブジェクトパターンで item_id を追加 ▲ #
+    @order_address = OrderAddress.new
   end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: 422
+    end
+  end
+
+    private
+
+    def order_address_params
+      params.require(:order_address).permit(:post_code, :city, :address_no, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: @item.id)
+    end
 
 end
