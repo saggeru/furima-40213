ccc713dd91733872ca04e1f706ec066b4703b5fe
saggeru <ylobb99@gmail.com>
2024-01-11 00:54:43 +0900

    Orderモデル・Addressモデルへのバリデーションを定義

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,4 +3,14 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :post_code, :city, :address_no, :building_name, :phone_number, :prefecture_id, :user_id, :item_id
 
+  # ▼ バリデーション ▼ #
+  with_options presence: true do
+    validates :user_id, :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Only enter in japanese"}
+    validates :address_no, format: {with: /\A[ぁ-んァ-ン一-龥0-9０-９]+\z/, message: "is invalid."}
+    validates :phone_number, format: {with: /\A[0-9]+\z/, length: 10..13}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  end
+
 end
