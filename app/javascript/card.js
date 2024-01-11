bb4f4dae18f2610e357132a1b1b6cb48f5215081
saggeru <ylobb99@gmail.com>
2024-01-11 14:13:15 +0900

    トークンを生成し、サーバーサイドへ送る処理

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -10,6 +10,23 @@ const pay = () => {
   expiryElement.mount('#expiry-form'); /* ▼▲ 生成したフォームをビューファイルに描画していく ▲▼ */
   cvcElement.mount('#cvc-form');
 
+  const form = document.getElementById('charge-form') /* ■ フォームを取得(=送信ボタンを押したときの保存先を取得) */
+  form.addEventListener("submit", (e) => {
+    payjp.createToken(numberElement).then(function (response) { /* ■ Payjpインスタンス createTokenでtokenを取得 ■ */
+      if (response.error) { /* ■ トークンが正常に生成されたか。 生成された場合はresponse.idが戻り値 ■ */
+      } else {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`; /* ■ token = response.id なおtokenはユーザーに見せる必要がないため'hidden'を指定 ■ */
+        renderDom.insertAdjacentHTML("beforeend", tokenObj); /* ■ フォームの直後にtokenを設置 ■ */
+      }
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
+      document.getElementById("charge-form").submit(); /* ■ カードの情報は上記で空。submit→サーバーサイドへ送られるのはtokenのみ ■ */
+    });
+    e.preventDefault();
+  });
 };
 
 window.addEventListener("turbo:load", pay);
