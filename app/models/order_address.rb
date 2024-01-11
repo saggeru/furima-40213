327889daf364379bb6b833b24e24fb37f3d973a5
saggeru <ylobb99@gmail.com>
2024-01-11 22:44:58 +0900

    電話番号 バリデーションを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,7 +8,7 @@ class OrderAddress
     validates :address_no, :user_id, :item_id, :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Only enter in japanese"}
-    validates :phone_number, format: {with: /\A[0-9]+\z/, length: 10..13}
+    validates :phone_number, format: { with: /\A[0-9]+\z/ }, length: { in: 10..11 }
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   end
 
