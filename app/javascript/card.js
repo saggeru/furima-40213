d749a699948de2e0422024e13f103240d51ff49a
saggeru <ylobb99@gmail.com>
2024-01-11 14:12:48 +0900

    カード情報 入力フォームを生成+設置

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,15 @@
 const pay = () => {
   const publicKey = document.getElementById('payjp-public-key').dataset.key;
   const payjp = Payjp(publicKey)
-}
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber'); /* ▼ 入力フォームの生成 ▼ */
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc'); /* ▲ ('引数')にはフォームのタイプ ▲ */
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form'); /* ▼▲ 生成したフォームをビューファイルに描画していく ▲▼ */
+  cvcElement.mount('#cvc-form');
+
+};
+
 window.addEventListener("turbo:load", pay);
