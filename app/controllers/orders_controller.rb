ba4826f740613e0d72f8f7d0beb1de1acd19a104
saggeru <ylobb99@gmail.com>
2024-01-11 16:47:41 +0900

    pay_itemを定義 決済可能に

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,16 +11,26 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
       @order_address.save
+      pay_item.save
       redirect_to root_path
     else
       render :index, status: 422
     end
   end
 
-    private
+  private
 
-    def order_address_params
-      params.require(:order_address).permit(:post_code, :city, :address_no, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: @item.id)
-    end
+  def order_address_params
+    params.require(:order_address).permit(:post_code, :city, :address_no, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_address_params[:token],
+        currency: 'jpy'
+      )
+  end
 
 end
