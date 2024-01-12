5d7a0f6a2e4a9904c9b21236f6256e493677af00
saggeru <ylobb99@gmail.com>
2024-01-12 17:00:48 +0900

    price.js 修正(再レンダリングされた時発火&計算結果)

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -7,8 +7,9 @@ function price_calc() {
     const profit = document.getElementById("profit");
 
     addTaxPrice.innerHTML = Math.floor(inputValue * 0.1);
-    profit.innerHTML = Math.floor(inputValue - inputValue * 0.1);
+    profit.innerHTML = Math.floor(inputValue - inputValue % 10);
   })
 }
 
 window.addEventListener('turbo:load', price_calc)
+window.addEventListener('turbo:render', price_calc)
